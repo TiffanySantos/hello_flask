8897c0af49e83651ecfd4b93db27a20c8c0ce430
Tiffany Santos <tiffanysantos@Tiffanys-MacBook-Air.local>
2020-08-07 17:44:17 +0200

    hello and api routes added

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, !"
+    return "Hello, Flask!"
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
